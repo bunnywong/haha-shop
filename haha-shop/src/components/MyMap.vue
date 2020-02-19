d077c9c3ce9a4ddb7c2b0f1b032e1fb4cd8f4e2f
BunnY <w@bunnywong.com>
2020-02-19 22:41:30 +0800

    add form with autocomplete input

--- haha-shop/src/components/MyMap.vue
+++ haha-shop/src/components/MyMap.vue
@@ -1,6 +1,37 @@
 <template>
   <div class="mymap">
-    <div class="mymap__form-wrapper">
+    <div class="mymap__form-wrapper" ref="mymapFormWrapper">
+      <form class="mymap__form">
+        <div class="mymap__form-starting-location">Starting location</div>
+        <gmap-autocomplete
+          class="mymap__form-text-input"
+          placeholder=""
+          :value="startingLocation"
+          :select-first-on-enter="true"
+          @place_changed="setStartingLocation"
+          ref="startingLocation">
+        </gmap-autocomplete>
+        <span class="mymap__form-cleaner-cross" v-show="startingLocation" v-on:click="startingLocation = ''">x</span>
+        <div class="mymap__form-drop-off-point">Drop-off point</div>
+        <gmap-autocomplete
+          class="mymap__form-text-input"
+          placeholder=""
+          :value="dropoffPoint"
+          :select-first-on-enter="true"
+          @place_changed="setDropoffPoint"
+          ref="dropoffPoint">
+        </gmap-autocomplete>
+        <span class="mymap__form-cleaner-cross" v-show="dropoffPoint" v-on:click="dropoffPoint = ''">x</span>
+        <div class="mymap__form-bottom-wrapper">
+          <div v-show="message" class="mymap__form-message" :class="{red: isWarningClass}">{{ message }}</div>
+          <div v-show="totalDistance">total distance: {{ totalDistance }}</div>
+          <div v-show="totalTime">total time: {{ totalTime }}</div>
+          <div class="mymap__form-button-wrapper">
+            <input type="reset" class="mymap__form-button" value="Reset" v-on:click="startingLocation = ''; dropoffPoint = ''; message = ''; totalTime = ''; totalDistance = ''">
+            <input type="submit" class="mymap__form-button" :value="totalTime ? 'Re-submit' : 'Submit'" @click="formSubmit()">
+          </div>
+        </div>
+      </form>
     </div>
     <div class="mymap__map-wrapper">
       <gmap-map
@@ -29,14 +60,29 @@ export default {
   },
   data () {
     return {
+      startingLocation: '',
+      dropoffPoint: '',
+      message: '',
+      totalDistance: '',
+      totalTime: '',
       mapZoom: 11,
       mapCenter: this.mapCenterRef,
-      mapCounter: 0
+      mapCounter: 0,
+      isWarningClass: false
     }
   },
-  computed: {
-  },
   methods: {
+    setStartingLocation (place) {
+      this.startingLocation = place.formatted_address
+    },
+    setDropoffPoint (place) {
+      this.dropoffPoint = place.formatted_address
+    },
+    formSubmit () {
+      this.totalDistance = this.totalTime = ''
+      if (this.startingLocation && this.dropoffPoint) {
+      }
+    }
   }
 }
 </script>
