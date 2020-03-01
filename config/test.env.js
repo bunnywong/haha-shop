216fa21d8c8fab5a99b14e84eb9560cfd0d888af
BunnY <w@bunnywong.com>
2020-03-01 23:41:12 +0800

    clean up env var

--- config/test.env.js
+++ config/test.env.js
@@ -3,7 +3,5 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  MY_GOOGLE_API_KEY: '"YOUR-GOOGLE-API-KEY"',
-  MOCK_API_DOMAIN: '"YOUR-MOCK-API-DOMAIN"'
+  NODE_ENV: '"testing"'
 })
